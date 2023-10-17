44127781478c2480f72459eccc948d4d3dd136a4
Yamen Alazwary <146210428+Yoms24@users.noreply.github.com>
2023-10-17 15:43:42 +0000

    Week 2

---
+++ Week2_C_Bootcamp/Question1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a = 1;
+    
+    for (a = 1; a <= 10; a++)
+    {
+        
+        if (a % 2 == 1)
+        {
+        printf ("%d\n", a);
+
+        }
+    }
+    
+
+    return (0);
+
+}
