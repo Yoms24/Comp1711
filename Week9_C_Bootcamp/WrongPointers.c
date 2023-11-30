4d7ae128a003c41ad610436863b00f881d551b13
Yamen Alazwary <146210428+Yoms24@users.noreply.github.com>
2023-11-30 09:53:47 +0000

    Change

---
+++ Week9_C_Bootcamp/WrongPointers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+struct list {
+    int data;
+    struct list *next_element;
+    };
+
+    int main()
+    {
+        struct list node;
+        // Learn from the error.
+        // Why the error.
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
